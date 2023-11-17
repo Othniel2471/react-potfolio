87834b2b6d461a9ca3583e91d486b78fe365588f
othniel2471 <othnielomaidu@gmail.com>
2023-11-17 23:27:28 +0100

    added contact form

--- src/components/sections/ContactSection.jsx
+++ src/components/sections/ContactSection.jsx
@@ -6,6 +6,40 @@ import copyIcon from '../../assets/svgs/copy-icon.svg';
 const ContactSection = () => {
   const [isCopied, setIsCopied] = useState(false);
   const [showCopiedAlert, setShowCopiedAlert] = useState(false);
+  const [showSubmitAlert, setShowSubmitAlert] = useState(false);
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const data = {};
+    formData.forEach((value, key) => {
+      data[key] = value;
+    });
+
+    try {
+      const response = await fetch('https://formspree.io/f/mpzebpge', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (response.ok) {
+        setIsFormSubmitted(true);
+        setShowSubmitAlert(true);
+
+        setTimeout(() => {
+          setShowSubmitAlert(false);
+        }, 1000);
+      } else {
+        console.error('Form submission failed:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error submitting form:', error);
+    }
+  };
 
   const copyEmail = async () => {
     if (isCopied) return;
@@ -42,6 +76,46 @@ const ContactSection = () => {
         </div>
         <div className="flex flex-col items-center gap-6 md:gap-12">
           <div className="flex flex-col items-center md:gap-4">
+            <div className="flex items-center gap-4 md:gap-5">
+              <form
+                action="https://formspree.io/f/mpzebpge"
+                method="POST"
+                onSubmit={handleSubmit}
+                className="flex flex-col items-center gap-4 md:gap-5"
+              >
+                <input
+                  type="text"
+                  placeholder="Name"
+                  className="w-full md:w-96 border border-gray-300 rounded-lg px-4 py-2 text-sm"
+                  required
+                />
+                <input
+                  type="email"
+                  placeholder="Email"
+                  className="w-full md:w-96 border border-gray-300 rounded-lg px-4 py-2 text-sm"
+                  required
+                />
+                <textarea
+                  name="message"
+                  id="message"
+                  cols="30"
+                  rows="10"
+                  placeholder="Message"
+                  className="w-full md:w-96 border border-gray-300 rounded-lg px-4 py-2 text-sm"
+                  required
+                ></textarea>
+                <button
+                  type="submit"
+                  disabled={isFormSubmitted}
+                  className="inline-flex items-center w-20 justify-center rounded-xl bg-gray-900 px-4 py-1.5 font-medium text-gray-50 transition-colors duration-200 hover:bg-gray-700 active:bg-gray-800"
+                >
+                  {isFormSubmitted ? 'Sent' : 'Send'}
+                </button>
+                {showSubmitAlert && (
+                  <p className="text-green-500">Message sent!</p>
+                )}
+              </form>
+            </div>
             <div className="flex items-center gap-4 md:gap-5">
               <FontAwesomeIcon
                 icon={faEnvelope}
